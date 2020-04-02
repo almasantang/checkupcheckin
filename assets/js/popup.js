c44dd0407a9869a3de7ee1d4cceb3f2ce99efb31
Alma Santang <30685704+almasantang@users.noreply.github.com>
2020-04-02 20:19:41 +1100

    js update baybeee

---
+++ assets/js/popup.js
@@ -0,0 +1,23 @@
+$(	document ).ready(function()	{
+	// start here
+
+
+// popup
+$('.popup-link').click(function() {
+	$('.popup').addClass('active');
+});
+
+$('.popup').click(function() {
+	$('.popup').removeClass('active');
+});
+
+var headline = document.getElementById("clickHeadline");
+headline.onclick = function() {
+	headline.innerHTML="You have no unread message :-)";
+	}
+headline.onclick = function() {
+	document.getElementById("clickHeadline").style.color = 'black';
+}
+
+
+});
